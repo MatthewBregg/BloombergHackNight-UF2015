d5ab3c60e9f1aa763419ab55d3cc9a695ea98975
mbregg <matthewbregg610@ufl.edu>
2015-10-23 20:56:12 -0400

    Put up simple status loop for us

---
+++ statusloop.py
@@ -0,0 +1,18 @@
+import clientpy3
+import time
+import subprocess
+import platform
+
+#From http://stackoverflow.com/questions/2084508/clear-terminal-in-python
+def clear():
+    subprocess.Popen( "cls" if platform.system() == "Windows" else "clear", shell=True)
+
+
+while(True):
+        print("Cash is : " + str(clientpy3.run("___","____","MY_CASH")))
+        print("Securities are : "
+              + str(clientpy3.run("___","____","MY_SECURITIES")))
+        print("My orders are are : " + str(clientpy3.run("___","____","MY_ORDERS")))
+        time.sleep(1)
+        clear()
+
