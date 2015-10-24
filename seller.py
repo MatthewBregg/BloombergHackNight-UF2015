8697366bdc0f0032eec21efb4a01f7160fe062c2
mbregg <matthewbregg610@ufl.edu>
2015-10-24 01:33:34 -0400

    Fixed deriving

--- seller.py
+++ seller.py
@@ -6,31 +6,44 @@ def getDeriveArray(input, days):
     for day in input:
         accumulation.append({})
            
-    for key, value in input[0]:
+    for key in input[0]:
         bidAc = 0
         askAc = 0
         worthAc = 0
         days = 0
         for day, accum_day in zip(input,accumulation):
-            ++days
+            days += 1
+            # print(days)
+            accum_day[key] = {"bid":0,"ask":0,"net_worth":0}
             bidAc += day[key]["bid"]
             accum_day[key]["bid"] = bidAc/days
 
             askAc += day[key]["ask"]
             accum_day[key]["ask"] = askAc/days
 
-            worthAc += day[key]["worth"]
-            accum_day[key]["worth"] = worthAc/days
+            worthAc += day[key]["net_worth"]
+            accum_day[key]["net_worth"] = worthAc/days
     return accumulation
 
 
 logData = []
-def calculateSale():
-    logData.append(RecordHistory.record())
-    print(logData)
-    firstDx = getDeriveArray(logData, 10)
-    print(firstDx)
-    secondDx = getDeriveArray(firstDx, 10)
 
+
+def calculateSale():
+    testdata = [
+        {'AAPL': {"bid":10,"ask":15,"net_worth":5},
+         'GOOGL':{"bid":1,"ask":5,"net_worth":7} },
+        {'AAPL': {"bid":15,"ask":10,"net_worth":3},
+         'GOOGL':{"bid":1,"ask":6,"net_worth":3}}]
+    print(getDeriveArray(testdata,2))
+    # for i in range(1,10):
+    #     logData.append(RecordHistory.record())
+    #     print(logData)
+    #     firstDx = getDeriveArray(logData, 10)
+    #     print("first dx")
+    #     print(firstDx)
+    #     secondDx = getDeriveArray(firstDx, 10)
+    #     print("Second dx")
+    #     print(secondDx)
 
 calculateSale()
