9b28a9fc72bc724b06b27e79c4224e500411555c
ryoung48 <ryoung48@ufl.edu>
2015-10-23 23:51:16 -0400

    history

--- RecordHistory.py
+++ RecordHistory.py
@@ -3,6 +3,8 @@ import socket
 from sklearn.externals import joblib
 import time
 import numpy as np
+import random
+import string
 
 
 def run(user, password, *commands):
@@ -24,19 +26,44 @@ def run(user, password, *commands):
         return result 
 
 #Records past prices for all tickers
-#Input: (rec) hash map <ticker,list_of_prices> and (pos) current position
+#Input: (rec) hash map <ticker,dict_of_prices> and (pos) current position
 #Output: current position
-def record(rec:dict,pos:int) -> int:
-    x = run("___","____", "SECURITIES")
-    securities = x.split(" ")
-    securities.pop(0)
-    for i in range(0,len(securities)):
-        if(securities[i] != '3.0E-4' and securities[i] != '0.005'):
-            if(len(securities[i]) > 1 and len(securities[i]) < 5):
-                price = rec[securities[i]]
-                price[pos] = float(securities[i+1])
-    pos = (pos+1) % 1000
-    return pos
-records =  {'AAPL': np.zeros(1000), 'ATVI': np.zeros(1000), 'EA': np.zeros(1000),'FB': np.zeros(1000), 'GOOG': np.zeros(1000), 'MSFT': np.zeros(1000), 'SBUX': np.zeros(1000), 'SNY': np.zeros(1000), 'TSLA': np.zeros(1000),'TWTR': np.zeros(1000)}
-pos = 0
-pos = record(records,pos)
+def record():
+    records =  {'AAPL': dict(), 'ATVI': dict(), 'EA': dict(),'FB': dict(), 'GOOG': dict(), 'MSFT': dict(), 'SBUX': dict(), 'SNY': dict(), 'TSLA': dict(),'TWTR': dict()}
+    pos = 0
+    while(True):
+
+        result = run("___","____", "SECURITIES")
+        securities = result.split(" ")
+        securities.pop(0)
+
+        dict_of_orders = dict()
+        for key in records.keys():
+            result = run("___","____", "ORDERS " + key)
+            result = result.split(" ")
+            records[key]['bid'] = float(result[3])
+            records[key]['ask'] = float(result[7]) 
+        for i in range(0,len(securities)):
+            if(securities[i].isalnum()):
+                name = securities[i]
+                net_worth = securities[i+1];
+                records[name]['net_worth'] = float(net_worth)                  
+        pos = pos+1
+        if(pos == 10):
+            joblib.dump(records, "history.pkl")
+            pos = 0
+        print(pos)
+
+
+
+def load_history(name):
+    records = joblib.load(name + ".pkl")
+    return records
+
+record()
+rec = load_history("history")
+re1 = load_history("85v8m")
+while(True):
+    print(pos)
+    pos = record(records,pos)
+    #time.sleep(0.01)
