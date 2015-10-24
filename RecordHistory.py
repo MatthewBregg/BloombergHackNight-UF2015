4c527d0947dee3284d8b28bd0b879b841faae719
ryoung48 <ryoung48@ufl.edu>
2015-10-24 00:00:43 -0400

    history_take2

--- RecordHistory.py
+++ RecordHistory.py
@@ -25,34 +25,27 @@ def run(user, password, *commands):
             rline = sfile.readline()
         return result 
 
-#Records past prices for all tickers
-#Input: (rec) hash map <ticker,dict_of_prices> and (pos) current position
-#Output: current position
+
 def record():
     records =  {'AAPL': dict(), 'ATVI': dict(), 'EA': dict(),'FB': dict(), 'GOOG': dict(), 'MSFT': dict(), 'SBUX': dict(), 'SNY': dict(), 'TSLA': dict(),'TWTR': dict()}
     pos = 0
-    while(True):
+    result = run("___","____", "SECURITIES")
+    securities = result.split(" ")
+    securities.pop(0)
 
-        result = run("___","____", "SECURITIES")
-        securities = result.split(" ")
-        securities.pop(0)
-
-        dict_of_orders = dict()
-        for key in records.keys():
-            result = run("___","____", "ORDERS " + key)
-            result = result.split(" ")
-            records[key]['bid'] = float(result[3])
-            records[key]['ask'] = float(result[7]) 
-        for i in range(0,len(securities)):
-            if(securities[i].isalnum()):
-                name = securities[i]
-                net_worth = securities[i+1];
-                records[name]['net_worth'] = float(net_worth)                  
-        pos = pos+1
-        if(pos == 10):
-            joblib.dump(records, "history.pkl")
-            pos = 0
-        print(pos)
+    dict_of_orders = dict()
+    for key in records.keys():
+        result = run("___","____", "ORDERS " + key)
+        result = result.split(" ")
+        records[key]['bid'] = float(result[3])
+        records[key]['ask'] = float(result[7]) 
+    for i in range(0,len(securities)):
+        if(securities[i].isalnum()):
+            name = securities[i]
+            net_worth = securities[i+1];
+            records[name]['net_worth'] = float(net_worth)                  
+    #joblib.dump(records, "history.pkl")
+    return records
 
 
 
@@ -60,10 +53,4 @@ def load_history(name):
     records = joblib.load(name + ".pkl")
     return records
 
-record()
-rec = load_history("history")
-re1 = load_history("85v8m")
-while(True):
-    print(pos)
-    pos = record(records,pos)
-    #time.sleep(0.01)
+rec44 = record()
